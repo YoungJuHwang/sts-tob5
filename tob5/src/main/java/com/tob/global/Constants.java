669e46eaebd7a765d87e5c0cad144c28155bb8c4
pheonix0717@naver.com <pheonix0717@naver.com>
2016-01-11 10:09:12 +0900

    2016-01-11  AM 10:09

--- tob5/src/main/java/com/tob/global/Constants.java
+++ tob5/src/main/java/com/tob/global/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 	public static final String ORACLE_ID = "movie";
 	public static final String ORACLE_PASSWORD = "movie";
 	public static final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
-	public static final String MYSQL_URL = "jdbc:mysql://localhost:3306/pheonix0717";
+	public static final String MYSQL_URL = "jdbc:mysql://192.168.0.125:3306/pheonix0717";
 	public static final String MYSQL_ID = "root";
 	public static final String MYSQL_PASSWORD = "mysql";
 	public static final String HSQL_DRIVER = "org.hsqldb.jdbcDriver";
